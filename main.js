7be486f28396951a546741998f7176e779ad4dd0
josempalencia <115725093+josempalencia@users.noreply.github.com>
2023-01-19 15:21:20 -0400

    se agregan correo

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const welcomeText = document.getElementById("welcome");
 function usernamePrompt() {
     // pedimos al usuario su nombre
     let username = prompt("Ingrese su nombre");
-
     if (!username) return;
     // verificamos que es un nombre valido (eg. no tiene simbolos)
     for (const letter of username) {
@@ -13,10 +12,14 @@ function usernamePrompt() {
             return;
         }
     }
+
+    let email = prompt("Ingrese su correo");
+
     // escondemos el boton
     idButton.style = "display: none;";
     // mostramos nombre en la pagina
     welcomeText.innerHTML = "Bienvenido " + username;
+    if (email) welcomeText.innerHTML += " (" + email + ")"
 }
 idButton.addEventListener("click", function (evt) {
     usernamePrompt();
