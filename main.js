112a0cb32e5a506442a7cc590730c396d265c4dc
josempalencia <115725093+josempalencia@users.noreply.github.com>
2023-04-15 23:38:45 +0200

    hago peuqueños cambios

--- main.js
+++ main.js
@@ -27,40 +27,40 @@ idButton.addEventListener("click", function (evt) {
   usernamePrompt();
 });
 
-const newsButton = document.getElementById("btn-news");
-const newsModal = document.getElementById("modal-news");
+// const newsButton = document.getElementById("btn-news");
+// const newsModal = document.getElementById("modal-news");
 
-async function fetchNews() {
-  const req = await fetch("https://api.spaceflightnewsapi.net/v3/articles?_limit=20");
-  if (req) {
-    const articles = await req.json();
-    const list = document.getElementById("news-list");
-    articles.forEach((article) => {
-      const container = document.createElement("li");
+// async function fetchNews() {
+//   const req = await fetch("https://api.spaceflightnewsapi.net/v3/articles?_limit=20");
+//   if (req) {
+//     const articles = await req.json();
+//     const list = document.getElementById("news-list");
+//     articles.forEach((article) => {
+//       const container = document.createElement("li");
       
-      const title = document.createElement("h4");
-      title.innerText = article.title;
-      container.appendChild(title);
+//       const title = document.createElement("h4");
+//       title.innerText = article.title;
+//       container.appendChild(title);
       
-      const content = document.createElement("p");
-      content.innerText = article.summary;
-      container.appendChild(content);
+//       const content = document.createElement("p");
+//       content.innerText = article.summary;
+//       container.appendChild(content);
       
-      const link = document.createElement("a");
-      link.href = article.url;
-      link.innerHTML = "Leer más"
-      container.appendChild(link);
+//       const link = document.createElement("a");
+//       link.href = article.url;
+//       link.innerHTML = "Leer más"
+//       container.appendChild(link);
   
-      list.appendChild(container);
-    });
+//       list.appendChild(container);
+//     });
 
-    $('#modal-news').modal('show');
-  } else {
-    alert("Ha habido un error en cargar las noticias. Intente de nuevo mas tarde.");
-  }
-}
+//     $('#modal-news').modal('show');
+//   } else {
+//     alert("Ha habido un error en cargar las noticias. Intente de nuevo mas tarde.");
+//   }
+// }
 
-newsButton.addEventListener("click", () => fetchNews());
+// newsButton.addEventListener("click", () => fetchNews());
 
 // $('#btn-news').on('click', function () {
 //   // $.ajax({
@@ -85,3 +85,79 @@ newsButton.addEventListener("click", () => fetchNews());
 //   //   }
 //   // });
 // });
+// URL de la API de OpenAI
+const apiUrl = 'https://api.openai.com/v1/completions';
+
+// Parámetros de la petición
+const data = {
+  'prompt': '',
+  'max_tokens': 100,
+  'temperature': 0.5,
+  'model': 'curie'
+};
+
+// Encabezados de la petición
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${'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'}`
+};
+
+// Obtener los elementos del DOM
+const openChatButton = document.getElementById('open-chat');
+const chatModal = document.getElementById('chat-modal');
+const closeChatButton = document.getElementsByClassName('close')[0];
+const questionForm = document.getElementById('question-form');
+const questionInput = document.getElementById('question-input');
+const answerDiv = document.getElementById('answer');
+
+// Función para mostrar el modal de chat
+function openChatModal() {
+  chatModal.style.display = 'block';
+}
+
+// Función para ocultar el modal de chat
+function closeChatModal() {
+  chatModal.style.display = 'none';
+}
+
+async function askQuestion(question) {
+	// Actualizar la pregunta en los datos de la petición
+	data.prompt = `Pregunta sobre seguros: ${question}`;
+
+	try {
+		// Realizar la petición a la API
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			headers: headers,
+			body: JSON.stringify(data)
+		});
+
+		// Obtener la respuesta como JSON
+		const answer = await response.json();
+
+		// Limpiar respuesta anterior
+		answerDiv.innerHTML = "";
+
+		// Mostrar la respuesta en la página
+		answerDiv.innerHTML = answer.choices[0].text;
+	} catch (error) {
+		console.error(error);
+		answerDiv.innerHTML = 'Error al obtener la respuesta.';
+	}
+}
+
+
+// Manejador de eventos para mostrar el modal de chat
+openChatButton.addEventListener('click', openChatModal);
+
+// Manejador de eventos para ocultar el modal de chat
+closeChatButton.addEventListener('click', closeChatModal);
+
+// Manejador de eventos para enviar una pregunta
+questionForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const question = questionInput.value.trim();
+  if (question !== '') {
+    askQuestion(question);
+  }
+});
